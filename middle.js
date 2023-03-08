a0cfc1618f4ae469352896c1ad24059d71cf5eb8
Jeremy <010JGL@GMAIL.COM>
2023-03-08 15:23:58 -0500

    added comments

--- middle.js
+++ middle.js
@@ -16,15 +16,15 @@ const assertArraysEqual = function(actual, expected) {
 
 // take in an array, returns the middle most elements
 
-const middle = function(array) {
-  if (array.length < 3) {
-    return [];
-  } else if (array.length % 2 === 0) {
-    return array.slice(array.length / 2 - 1, array.length / 2 + 1)
-  } else {
-    let newArray = [];
-    let middleNum = Math.round(array.length / 2);
-    newArray.push(array[middleNum - 1]);
+const middle = function(array) {                              
+  if (array.length < 3) {                                          //returns [] when array length is under 3
+    return [];  
+  } else if (array.length % 2 === 0) {                             //if array length is even
+    return array.slice(array.length / 2 - 1, array.length / 2 + 1)  // slice from array *slice(start, end)*
+  } else {                                                          // is odd
+    let newArray = [];                                              // declare new array to return after
+    let middleNum = Math.round(array.length / 2);                   // if odd middle is 7 / 2 = 3.5   3.5 rounded is 4. 
+    newArray.push(array[middleNum - 1]);                            // 4 - 1 to get the index to return
     return newArray;
   }
   
@@ -39,5 +39,5 @@ const middle = function(array) {
 ///console.log(middle([1, 2, 3, 4])) // => [2, 3]
 //console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
 
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
+console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));  // middle has to have arguments
 //                            actual                     expected
