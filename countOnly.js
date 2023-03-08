25e148715caa5096bc1cc00f5d0a799e05650168
Jeremy <010JGL@GMAIL.COM>
2023-03-08 17:05:07 -0500

    adde countOnly function

---
+++ countOnly.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {         // checks the array, items to compare with
+  const results = {}                                         // creat var to return
+  
+  for (const item of allItems) {                             // for of loop. looks for items in our array
+        
+  if (itemsToCount[item]) {                                  // checks for single item from allItems, in items to count  
+      if (results[item]) {                                   // if item is found add 1 to results
+        results[item] += 1;
+      }   else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
