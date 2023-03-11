b7143e88d9fc6f0f0fb8fe683b41169dc7bd4f96
Jeremy <010JGL@GMAIL.COM>
2023-03-11 13:31:24 -0500

    TakeUnil exercise. Returning a modified array when we find a falsey

--- takeUntil.js
+++ takeUntil.js
@@ -1,53 +1,44 @@
-// takes 2 parameters
-// Array & callback
-//const newArray = [];
-//const array = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const eqArrays = function(a, b, c) {
+  c = JSON.stringify(a) === JSON.stringify(b);
+  return c;
+};
 
 
-/*const takeUntil = function(arr, callback) {
-  for (let item of arr) {
-    console.log(item);
-    if (item == true) {
-    console.log(item);
-      newArray.push(callback(item));
-    }
-    else {
-      break;
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) == true) {        // compares the function return with true
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  }
+  else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+  return eqArrays(actual, expected);         // can use a function as a return
+};
+// Return a slice of the array with elements taken from beginning. until we finda truthy value
+
+
+const takeUntil = function(array, callback) {   // takes one parameter and one call back
+  
+  for (let i in array) {              // loop thru all of the array
+    if (callback(array[i])) {         // if callback(condition) of the index were at in the array
+      array.splice(i);                // splice the array at that point
+      return array;                   // return a modified array
     }
   }
-  return console.log(newArray);
-}
-
-const result1 = takeUntil(array, value => value);
-console.log(result1);
-*/
-const newArray = [];
-//const array = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-
-const takeUntil = function(array, callback) {
-  for (let arr of array) {
-    if (arr == true) {
-     return callback(arr);
-    }
-    else {
-      break;
-    }
-  }
-  return console.log(newArray);
 
 };
 
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-//console.log(results1);
-
-//console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-//console.log(results2);
 
 //[ 1, 2, 5, 7, 2 ]
 //--
 //[ 'I\'ve', 'been', 'to', 'Hollywood' ]
+console.log(results1);
+console.log(results2);
+
+assertArraysEqual(data1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(data2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
