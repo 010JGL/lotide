872d91bc026e76a41faf10c243c591ae6d25d145
Jeremy <010JGL@GMAIL.COM>
2023-03-13 15:08:15 -0400

    add test for middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -9,5 +9,7 @@ describe("#middle", () => {
   it("returns [2] for [1, 2, 3]", () => {
     assert.deepEqual(middle([1, 2, 3]), [2]);
   });
-
+  it("returns [3] for [1, 3, 5]", () => {
+    assert.deepEqual(middle([1, 3, 5]), [3]);
+  });
 });
